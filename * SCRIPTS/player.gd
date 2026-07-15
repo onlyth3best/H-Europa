19299a7ac5430f0081c3654635e639efd4c4d795
01ghostboy10 <levana.rai.simcik@mensa.cz>
2026-07-15 15:34:34 +0200

    finished the sky and the player finally works w/ mouse movement

--- * SCRIPTS/player.gd
+++ * SCRIPTS/player.gd
@@ -1,9 +1,19 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 3.5
 
+@export var camera : Camera3D
+var mouse_sensitivity := 0.1
+var rotation_x := 0
+var rotation_y := 0
+
+func _process(_delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,7 +26,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -26,3 +36,13 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotation_y -= event.relative.x * mouse_sensitivity
+		rotation_x -= event.relative.y * mouse_sensitivity
+		
+		rotation_x = clamp(rotation_x, -90, 90)
+		
+		rotation_degrees.y = rotation_y
+		camera.rotation_degrees.x = rotation_x
