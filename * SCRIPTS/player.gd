daac7bcf8ec71724f6f8ad1edbb63ba7704e9e01
01ghostboy10 <levana.rai.simcik@mensa.cz>
2026-07-15 12:29:35 +0200

    trying player movement

---
+++ * SCRIPTS/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
+
+
+func _physics_process(_delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+# in 3d XZ is GROUND PLANE
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		# Setting the basis property will affect the rotation of the node.
+		$Pivot.basis = Basis.looking_at(direction)
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration * _delta)
+		
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
